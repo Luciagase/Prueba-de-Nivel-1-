ab18675ebc4c91ff071b21208a5c0b5768781fee
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-03-06 21:42:47 +0100

    change

--- Herencias.py
+++ Herencias.py
@@ -48,10 +48,45 @@ class Camioneta(Coche):
         return Coche.__str__(self) + ", {} kg".format(self.carga)
     
 
+
+"""def catalogar(lista):
+    for vehiculo in lista:
+        print("{}".format(type(vehiculo).__name__)) 
+        #print(type(vehiculo).__name__) 
+        print(vehiculo, "\n")"""
+
+def catalogar(lista,ruedas=None):
+
+    if ruedas != None:
+        contador = 0
+        for vehiculo in vehiculos:
+            if vehiculo.ruedas == ruedas:
+                contador += 1
+        print("Se han encontrado {} vehiculos con {} ruedas: ".format(contador,ruedas))
+        
+
+    for vehiculo in lista:
+        if ruedas == None:
+            print("{} {}".format(type(vehiculo).__name__, vehiculo))
+        else:
+            if vehiculo.ruedas == ruedas:
+                print("{} {}".format(type(vehiculo).__name__,vehiculo))
+
+
 coche = Coche("rosa",4,240,650)
 camion = Camioneta("azul",6,170,800,5000)
 bici = Bicicleta("rojo",2,"urbana")
 moto = Motocicleta("gris",2,"urbana",195,350)
+coche1 = Coche("negro",4,200,640)
+camion1 = Camioneta("blanco",6,200,600,1000)
+bici1 = Bicicleta("marrón",2,"deportiva")
+moto1 = Motocicleta("gris",2,"urbana",180,250)
+coche2 = Coche("blanco",4,130,700)
+camion2 = Camioneta("gris",6,120,800,3000)
+bici2 = Bicicleta("azul",2,"urbana")
+moto2 = Motocicleta("negro",2,"deportiva",195,350)
 
 # Lista
-vehiculos = [coche, camion, bici, moto]
+vehiculos = [coche, camion, bici, moto, coche1, camion1, bici1, moto1, coche2, camion2, bici2, moto2]
+
+catalogar(vehiculos,2)
